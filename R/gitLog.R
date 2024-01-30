981f9f9b4c8717b7dd54d5549db8ee79e6d99739
michaelm <michaelm@metrumrg.com>
2024-01-30 15:00:09 -0500

    Update gitlog to use less imports

--- R/gitLog.R
+++ R/gitLog.R
@@ -5,7 +5,8 @@ gitLog <- function(file) {
   outList <- list()
   
   p <- processx::run("git", c("log", "--format=%H%x09%an%x09%aI%x09", "-n1", "--", file))
-  p_out <- purrr::flatten_chr(stringr::str_split(p[["stdout"]], "\t"))
+  #p_out <- purrr::flatten_chr(stringr::str_split(p[["stdout"]], "\t"))
+  p_out <- unlist(strsplit(p$stdout, split = "\t"))
   
   outList[["commit"]] <- p_out[1]
   outList[["author"]] <- p_out[2]
