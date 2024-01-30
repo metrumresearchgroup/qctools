3afc90972982a43dcf92f4ec3704e0efad6fcf3e
michaelm <michaelm@metrumrg.com>
2024-01-30 14:55:31 -0500

    Add logAccept using gitLog

---
+++ R/gitLog.R
@@ -0,0 +1,15 @@
+gitLog <- function(file) {
+  
+  file_abs <- fs::path_rel(path = file, start = logDir())
+  
+  outList <- list()
+  
+  p <- processx::run("git", c("log", "--format=%H%x09%an%x09%aI%x09", "-n1", "--", file))
+  p_out <- purrr::flatten_chr(stringr::str_split(p[["stdout"]], "\t"))
+  
+  outList[["commit"]] <- p_out[1]
+  outList[["author"]] <- p_out[2]
+  outList[["datetime"]] <- p_out[3]
+  
+  return(outList)
+}
---
+++ R/logAccept.R
@@ -0,0 +1,23 @@
+#' Approve a File
+#' 
+#' @description 
+#' Add a row to the QC log accepting a file, recording the commit hash of the 
+#' commit where the file's last modification occurred.
+#' 
+#' @param file file to accept in the QC log
+#' 
+#' @usage 
+#' logAccept(file)
+#' 
+#' @export
+logAccept <- function(file){
+  
+  git_commit <- gitLog(file)
+  
+  logEdit(
+    file = file,
+    reviewer = Sys.info()[["user"]],
+    commit = git_commit$commit
+  )
+  
+}
