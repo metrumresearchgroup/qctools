38a137a2b7546d98d46efa3b5cb23ea301dc8a90
michaelm <michaelm@metrumrg.com>
2024-01-31 17:24:03 -0500

    Add demoGit for testing

---
+++ R/demoGit.R
@@ -0,0 +1,133 @@
+#' Create Demo Git Repo
+#' 
+#' @description 
+#' Create a Github repo with files checked in and a QC log. The purpose of this 
+#' demo repo is for the user to become familiar with using review functions. 
+#' 
+#' The files checked into the repo are at various stages in terms of their QC
+#' needs. This function returns the path to the repo, so that the user can set
+#' their working directory to it. 
+#' 
+#' This demo repo is created in the `tmp` folder, so a new one will need to be
+#' generated whenever the user restarts R. 
+#'
+#' @export
+demoGit <- function() {
+  
+  curDir <- getwd()
+  on.exit(setwd(curDir))
+  
+  repoInitPath <- fs::path_temp()
+  
+  if (fs::dir_exists(repoInitPath)) {
+    fs::dir_delete(repoInitPath)
+  }
+  
+  # Create svn repo at specified locations
+  system(glue::glue("git init {repoInitPath}"))
+  
+  setwd(repoInitPath)
+  
+  # Add scripts to the repo
+  fs::dir_create("script/pk", recurse = TRUE)
+  
+  writeLines(
+    c(
+      "library(tidyverse)",
+      'src_abc <- mrgda::read_src_dir(here::here("data", "source", "STUDY-ABC"))',
+      "derived <- list(sl = list(),tv = list())",
+      'dm_0 <- src_abc$dm %>% filter(ACTARM != "Screen Failure")',
+      "derived$sl$dm <- dm_0"
+    ),
+    "script/data-assembly.R"
+  )
+  
+  writeLines(
+    c(
+      "library(tidyverse)",
+      "studies <- list()",
+      'studies$pk_abc <- readr::read_rds(here::here("data", "derived", "studies", "pk-abc.rds"))',
+      "pk_0 <- bind_rows(studies) %>% arrange(USUBJID, DATETIME)",
+      'pk_1 <- pk_0 %>% mrgda::assign_id(., "USUBJID")',
+      "pk_out <- pk_1"
+    ),
+    "script/combine-da.R"
+  )
+  
+  writeLines(
+    c(
+      'pk_spec <- yspec::load_spec(here::here("script", "script/examp-yaml.yaml"))'
+    ),
+    "script/pk/load-spec.R"
+  )
+  
+  writeLines(c('This is the first version of the txt file'),
+             "script/examp-txt.txt")
+  
+  writeLines(c("This is the first version of the yaml file"),
+             "script/examp-yaml.yaml")
+  
+  # Create QC log
+  logCreate()
+  
+  # Check everything into SVN
+  system("git add *")
+  system(glue::glue("git commit -m 'initial commit'"))
+  
+  # Assign and accept scripts in QC log
+  logAssign("script/data-assembly.R")
+  logAssign("script/pk/load-spec.R")
+  logAssign("script/combine-da.R")
+  logAssign("script/examp-txt.txt")
+  
+  system("git add *")
+  system(glue::glue("git commit -m 'logAssign scripts ready for QC'"))
+  
+  logAccept("script/data-assembly.R")
+  logAccept("script/pk/load-spec.R")
+  logAccept("script/combine-da.R")
+  
+  # Check in updates to QC log
+  system("git add *")
+  system(glue::glue("git commit -m 'logAccept scripts ready for QC'"))
+  
+  # Make edits to QCed file
+  writeLines(
+    c('pk_spec <- yspec::load_spec(here::here("script", "examp-yaml.yaml"))'),
+    "script/pk/load-spec.R"
+  )
+  
+  system("git add *")
+  system(glue::glue("git commit -m 'modify load-spec script'"))
+
+  writeLines(
+    c(
+      "library(tidyverse)",
+      'source(here::here("script", "data-assembly", "da-functions.R"))',
+      'src_abc <- mrgda::read_src_dir(here::here("data", "source", "STUDY-ABC"))',
+      "derived <- list(sl = list(),tv = list())",
+      'dm_0 <- src_abc$dm %>% filter(ACTARM != "Screen Failure")',
+      "derived$sl$dm <- dm_0",
+      'pk_0 <- src_abc$pc %>% filter(PCTEST == "TEST OF INTEREST")',
+      "derived$tv$pc <- pk_0",
+      'ex_1 <- src_abc$ex %>% filter(EXTRT == "DRUG OF INTEREST")',
+      "derived$tv$dosing <- ex_1"
+    ),
+    "script/data-assembly.R"
+  )
+  
+  system("git add *")
+  system(glue::glue("git commit -m 'modify data-assembly'"))
+
+  writeLines(
+    c("The following tasks are suggested to gain familiarity with the review package:",
+      '- run `diffQCed()` on "script/pk/load-spec.R" and "script/data-assembly.R"',
+      '- run `renderQCSummary()`',
+      '- use `logAssign()` to add "script/examp-txt.txt" to the QC log',
+      '- run `logPending()` to see what scripts are in need of QC',
+      '- use `logAccept()` to sign off on any scripts with pending QC'),
+    "README.md"
+  )
+  
+  repoInitPath
+}
--- tests/testthat/test-logAssign.R
+++ tests/testthat/test-logAssign.R
@@ -2,40 +2,15 @@
 # - Add files to a QC log, confirm the user and commit are as expected
 # - Try to add the same file to the QC log twice, expect error
 
-temp_loc <- tempdir()
-original_wd <- getwd()
-setwd(temp_loc)
+setwd(demoGit())
+
+path_to_qc_log <- file.path(logDir(),"QClog.csv")
+log <- logRead(path_to_qc_log)
 
 test_that("logAssign adds files to QC log with expected values", {
   
-  logCreate()
-  
-  file1 <- tempfile(fileext = ".R")
-  file2 <- tempfile(fileext = ".R")
-  file3 <- tempfile(fileext = ".R")
-  
-  file.create(file1)
-  file.create(file2)
-  file.create(file3)
-  
-  logAssign(file1)
-  logAssign(file2)
-  logAssign(file3)
-  
-  path_to_qc_log <- file.path(logDir(),"QClog.csv")
-  log <- logRead(path_to_qc_log)
-  
-  expect_true(unique(log$commit) == "assigned")
-  expect_true(unique(log$reviewer) == "anyone")
-  
-  expect_equal(log$file[1], basename(file1))
-  expect_equal(log$file[2], basename(file2))
-  expect_equal(log$file[3], basename(file3))
-  
-  expect_error(logAssign(file1))
-  file.remove(file.path(logDir(), "QClog.csv"))
+  expect_true(unique(log$commit[log$reviewer == "anyone"]) == "assigned")
+  expect_true(unique(log$reviewer[log$commit == "assigned"]) == "anyone")
 })
 
-
-setwd(original_wd) 
 
