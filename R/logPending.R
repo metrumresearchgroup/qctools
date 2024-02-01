13d2a1908cd6a1dfd7d3dfe6ac6e7429c1e55432
michaelm <michaelm@metrumrg.com>
2024-02-01 14:37:14 -0500

    Add logPending

---
+++ R/logPending.R
@@ -0,0 +1,51 @@
+#' List Files that Require QC
+#' 
+#' @description 
+#' Checks the files in the QC log and returns those that are not up to date with
+#' QC, meaning files that have only been assigned or have had modifications since
+#' their last QC was completed. 
+#' 
+#' @export
+logPending <- function() {
+  
+  path_to_qc_log <- file.path(logDir(),"QClog.csv")
+  
+  if (!file.exists(path_to_qc_log)) {
+    stop("No QC log found", call. = FALSE)
+  }
+  
+  log <- logRead(path_to_qc_log)
+  
+  list_of_files <- unique(log$file)
+  
+  pending <- 
+    data.frame(
+      file=character(0),
+      author=character(0),
+      reviewer = character(0),
+      datetime=character(0)
+    )
+  
+  for (file.i in list_of_files) {
+    
+    file_info.i <- gitLog(file.i)
+    file_log.i <- log[log[["file"]] == file.i, ]
+    
+    if (file_log.i$commit[nrow(file_log.i)] == file_info.i$commit) {
+      next
+    }
+    
+    pending.i <-
+      data.frame(
+        file=file.i,
+        author=file_info.i$author,
+        reviewer = file_log.i$reviewer[nrow(file_log.i)],
+        datetime = as.Date(file_info.i$datetime)
+      )
+    
+    pending <- rbind(pending, pending.i)
+  }
+  
+  pending
+  
+}
