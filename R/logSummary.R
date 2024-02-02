508ba492841fbece3f7455c238e69d9d6200ffe2
michaelm <michaelm@metrumrg.com>
2024-02-02 10:49:11 -0500

    Add logSummary

---
+++ R/logSummary.R
@@ -0,0 +1,55 @@
+#' Returns QC Status of Files in QC log
+#' 
+#' @description 
+#' Checks the files in the QC log and returns their QC status. Statuses include:
+#' "Assigned, needs QC", "Modified, needs QC", and "Fully QCed".
+#' 
+#' @export
+logSummary <- function() {
+  
+  path_to_qc_log <- file.path(logDir(),"QClog.csv")
+  
+  if (!file.exists(path_to_qc_log)) {
+    stop("No QC log found", call. = FALSE)
+  }
+  
+  log <- logRead(path_to_qc_log)
+  
+  list_of_files <- unique(log$file)
+  
+  summary <- 
+    data.frame(
+      file=character(0),
+      status=character(0)
+    )
+  
+  for (file.i in list_of_files) {
+    
+    file_info.i <- gitLog(file.i)
+    file_log.i <- log[log[["file"]] == file.i, ]
+    
+    if (nrow(file_log.i) == 1 & all(file_log.i$commit == "Initial-Assignment")) {
+      status.i <- "Assigned, needs QC"
+    } else if (file_log.i$commit[nrow(file_log.i)] == file_info.i$commit) {
+      status.i <- "Fully QCed"
+    } else {
+      status.i <- "Modified, needs QC"
+    }
+    
+    
+    
+    
+    summary.i <-
+      data.frame(
+        file=file.i,
+        status=status.i
+      )
+    
+    rm(status.i)
+    
+    summary <- rbind(summary, summary.i)
+  }
+  
+  summary
+  
+}
