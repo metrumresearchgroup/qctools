72399f309f5f1b361acbb9e7bda0c081c437faf1
michaelm <michaelm@metrumrg.com>
2024-04-17 13:42:07 -0400

    Add wd to processx

--- R/get-vcs.R
+++ R/get-vcs.R
@@ -1,13 +1,17 @@
 #' @keywords internal
 get_vcs <- function() {
   
-  svnCheck <- tryCatch(processx::run("svn", c("info"), error_on_status = FALSE))
+  svnCheck <- tryCatch(processx::run("svn", c("info"), 
+                                     wd = logDir(),
+                                     error_on_status = FALSE))
   
   if (svnCheck$status == 0) {
     return("svn")
   } 
   
-  gitCheck <- tryCatch(processx::run("git", c("log"), error_on_status = FALSE))
+  gitCheck <- tryCatch(processx::run("git", c("log"), 
+                                     wd = logDir(),
+                                     error_on_status = FALSE))
   
   if (gitCheck$status == 0) {
     return("git")
