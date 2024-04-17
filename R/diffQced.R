179f5243c79d2bea1f67119b4ae3b16d775acadf
michaelm <michaelm@metrumrg.com>
2024-04-17 13:24:08 -0400

    Update diffQced for svn

--- R/diffQced.R
+++ R/diffQced.R
@@ -15,6 +15,8 @@
 #' @export
 diffQced <- function(file) {
   
+  vcs <- get_vcs()
+  
   # Modify file to correct path format
   file_abs <- fs::path_abs(path = file)
   
@@ -32,7 +34,7 @@ diffQced <- function(file) {
   if (length(log_file) == 0) {
     stop(paste0(file, " not in QC log"), call. = FALSE)
   }
-  
+
   version_new <- vcsLastCommit(file)[["last_commit"]]
   version_qc <- log_file[length(log_file)]
   
@@ -40,20 +42,33 @@ diffQced <- function(file) {
     stop("File is up to date with QC", call. = FALSE)
   }
   
-  # Prepend "./" so that 'git cat-file' interprets the path relative to the
-  # working directory rather than the top-level directory of the Git repo.
-  commit_file_new <- paste0(version_new, ":./", file_rel)
-  commit_file_qc <- paste0(version_qc, ":./", file_rel)
-  
   tempfile_new <- file.path(tempdir(), paste0("new-", basename(file_rel)))
   tempfile_qc <- file.path(tempdir(), paste0("qced-", basename(file_rel)))
   
-  processx::run(
-    "git", c("cat-file", "blob", commit_file_new),
-    stdout = tempfile_new, wd = logDir())
-  processx::run(
-    "git", c("cat-file", "blob", commit_file_qc),
-    stdout = tempfile_qc, wd = logDir())
+  if (vcs == "git") {
+    
+    # Prepend "./" so that 'git cat-file' interprets the path relative to the
+    # working directory rather than the top-level directory of the Git repo.
+    commit_file_new <- paste0(version_new, ":./", file_rel)
+    commit_file_qc <- paste0(version_qc, ":./", file_rel)
+    
+    processx::run(
+      "git", c("cat-file", "blob", commit_file_new),
+      stdout = tempfile_new, wd = logDir())
+    processx::run(
+      "git", c("cat-file", "blob", commit_file_qc),
+      stdout = tempfile_qc, wd = logDir())
+  }
+  
+  if (vcs == "svn") {
+    
+    processx::run("svn", c("export", file_rel, paste0("-r", version_new), tempfile_new),
+                  wd = logDir())
+    
+    processx::run("svn", c("export", file_rel, paste0("-r", version_qc), tempfile_qc),
+                  wd = logDir())
+    
+  }
   
   diffobj::diffFile(
     target = tempfile_qc,
--- tests/testthat/test-diffQced.R
+++ tests/testthat/test-diffQced.R
@@ -55,3 +55,48 @@ test_that("diffQced works with subdirectories", {
   
   
 })
+
+test_that("diffQced works with subdirectories in SVN", {
+  
+  with_demoRepo({
+    
+    x <- diffQced("script/data-assembly.R")
+    
+    expect_equal(length(x@target) + 5, length(x@current))
+    expect_equal(x@tar.dat$orig[1], x@cur.dat$orig[1])
+    
+    y <- diffQced("script/pk/load-spec.R")
+    
+    expect_true(y@tar.dat$orig[1] != y@cur.dat$orig[1])
+    
+    # Create subdirectory
+    fs::dir_create("subdir")
+    setwd("subdir")
+    
+    # Add QC log & Rproj file
+    writeLines("Version: 1.0", con = "temp2.Rproj")
+    logCreate()
+    
+    # Create script directory
+    fs::dir_create("script")
+    writeLines("Example text", con = "script/subdir2.txt")
+    
+    # Check in new files to git
+    processx::run("git", c("add", "."))
+    processx::run("git", c("commit", "-m", "'add subdir'", "--quiet"))
+    logAccept("script/subdir2.txt")
+    
+    writeLines("New text", con = "script/subdir2.txt")
+    
+    processx::run("git", c("add", "."))
+    processx::run("git", c("commit", "-m", "'add subdir'", "--quiet"))
+    
+    # Check diff is possible while inside subdirectory
+    diff_sub <- diffQced("script/subdir2.txt")
+    
+    expect_true(diff_sub@target == "Example text")
+    expect_true(diff_sub@current == "New text")
+  })
+  
+  
+})
